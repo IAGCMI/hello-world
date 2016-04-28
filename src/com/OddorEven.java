a2487358362360ee2f33dbe897141d2773039cee
926120 <gayathri.a4@tcs.com>
2016-04-28 19:01:27 +0530

    merged commit

--- src/com/OddorEven.java
+++ src/com/OddorEven.java
@@ -2,13 +2,16 @@ package com;
 
 import java.util.Scanner;
 
+
 public class OddorEven {
+	
 	
 	public static void main(String args[])
 	{
 		Scanner c=new Scanner(System.in);
 	    Integer number;
-	    //String choice;
+	    int answer;
+	    int minus;
 		System.out.println("Odd or Even");
 		
 		System.out.println("Please Enter a number:");  
@@ -16,17 +19,25 @@ public class OddorEven {
 		if(number%2==0)
 		{
 			System.out.println("The number " + number + " is even");
-			int answer=multiply(number);
-			System.out.println(answer);
 		}
 		else
 		{
 			System.out.println("The number " + number +" is odd");
 		}
+		 answer=add(5,3);	
+		 System.out.println("The answer is : " + answer);
+		 minus=sub(5,3);	
+		 System.out.println("The minus is : " + minus);
+		 //test for conflict
 		
 	}
-	private static int multiply(int number){
-		return number*2;
+	private static int add(int a,int b)
+	{
+	return a+b;
 	}
-
+	private static int sub(int a,int b)
+	{
+	return a-b;
+	}
+	//test conflict gayathri
 }
